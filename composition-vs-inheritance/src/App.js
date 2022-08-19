0376b403ab560c2fc18224c3fd804323be767990
Kazi Ejaj <kejaj.777@gmail.com>
2022-08-19 11:29:33 +0600

    composition-vs-inheritance

---
+++ composition-vs-inheritance/src/App.js
@@ -0,0 +1,19 @@
+import Bracket from './components/composition/Bracket';
+import Emoji from './components/composition/Emoji';
+import Text from './components/composition/Text';
+
+function App() {
+  return (
+    <div>
+       <Emoji>
+            {({ addEmoji }) => (
+                <Bracket>
+                    {({ addBracket }) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
+                </Bracket>
+            )}
+        </Emoji>
+    </div>
+  );
+}
+
+export default App;
---
+++ composition-vs-inheritance/src/components/inheritance/Text.js
@@ -0,0 +1,11 @@
+import Emoji from './Emoji';
+
+export default class Text extends Emoji {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const decoratedText = this.addEmoji('I am JavaScript Language', '💜');
+        return super.render(decoratedText);
+    }
+}
