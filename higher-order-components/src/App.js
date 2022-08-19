4c32105e853bfacc9b99f7f3ed9f21a78e58e7e4
Kazi Ejaj <kejaj.777@gmail.com>
2022-08-19 12:13:30 +0600

    higher-order-components

---
+++ higher-order-components/src/App.js
@@ -0,0 +1,12 @@
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
+function App() {
+  return (
+    <div>
+      <ClickCounter/>
+      <HoverCounter/>
+    </div>
+  );
+}
+
+export default App;
